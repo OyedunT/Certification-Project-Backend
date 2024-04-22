f5181b598384cf5554bf1adfb2fa1f7e23f9756d
Titilope <Titilope>
2024-04-22 12:38:39 +0100

    FIRST COMMIT

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const connectDb = require("./Config/dbConfig");
+const env = require("dotenv").config()
+const userRoutes = require("./Routes/UserRoutes")
+const ErrorHandler = require("./Middlewares/ErrorHandlers")
+const app = express()
+
+
+
+
+
+app.use(express.json());
+
+app.use("/Api/User", userRoutes)
+
+app.use(ErrorHandler)
+
+console.log("Working Tree Clean");
+
+const port = process.env.PORT || 6000
+
+connectDb()
+
+app.listen(port, ()=>{
+    console.log(`App is Running on port ${port}`);
+})
